c0213a129fa7221016c47ff0acc14744e76b0e41
Caleb Bassi <calebjbassi@gmail.com>
2017-04-20 23:18:20 -0700

    Replaced screen with tmux

--- config_script.sh
+++ config_script.sh
@@ -6,5 +6,6 @@ mkdir ~/.vim/colors
 find ~/dotfiles -name '*rc' -exec ln -s {} ~/ ';'
 ln -s ~/dotfiles/*.vim ~/.vim/colors/
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 source ~/.bashrc
