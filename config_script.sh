6b8a95bfc123f25c6c8b643f334439a67bdecd2a
Caleb Bassi <calebjbassi@gmail.com>
2017-05-31 19:02:14 +0000

    Arch linux settings

--- config_script.sh
+++ config_script.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-mkdir ~/.vim/temp ~/.vim/undodir
-mkdir ~/.vim/colors
+dir="$HOME/dotfiles"
 
-find ~/dotfiles -name '*rc' -exec ln -s {} ~/ ';'
-ln -s ~/dotfiles/*.vim ~/.vim/colors/
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+mkdir ~/.vim/temp ~/.vim/undodir ~/.vim/colors
+
+ln -s $dir/config ~/.config/i3/
+
+sudo ln -s $dir/i3status.conf /etc/
+
+sudo ln -s $dir/50-mouse.conf /etc/X11/xorg.conf.d/
+
+ln -s $dir/solarizes.vim ~/.vim/colors/
+
+ln -s $dir/.bashrc ~/
+ln -s $dir/.gitconfig ~/
+ln -s $dir/.inputrc ~/
+ln -s $dir/.tmux.conf ~/
+ln -s $dir/.vimrc ~/
+ln -s $dir/.xinitrc ~/
 
 source ~/.bashrc
