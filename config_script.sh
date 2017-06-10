2b10ffeb37750163adc2c91f4a43595ab059ef41
Caleb Bassi <calebjbassi@gmail.com>
2017-06-09 17:54:49 -0700

    Colors, plugins, java compiling script

--- config_script.sh
+++ config_script.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# Vim directories
 mkdir ~/.vim/temp ~/.vim/undodir
 
+# Ranger config
 ln -s ~/config/rc.conf ~/.config/ranger/rc.conf
 
+# i3 config
 ln -s ~/config/X11/config ~/.config/i3/
 
+# i3 statusbar
 sudo ln -s ~/config/X11/i3status.conf /etc/
+
+# X11 mouse acceleration and speed
 sudo ln -s ~/config/X11/50-mouse.conf /etc/X11/xorg.conf.d/
 
+# Home directory config files
 ln -s ~/config/dotfiles/.* ~/
