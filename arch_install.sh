a89a72e85be67feb8256bc669f53b1414b25546a
Caleb Bassi <calebjbassi@gmail.com>
2017-07-09 15:15:16 -0700

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -63,7 +63,7 @@ ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 hwclock --systohc
 
 # Locale
-FIND="n_US.UTF-8 UTF-8" ; sed -i "s/#$FIND/$FIND/g" /etc/locale.gen
+FIND="en_US.UTF-8 UTF-8" ; sed -i "s/#$FIND/$FIND/g" /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 
