99462924b7861efad71a6551fdda3ecdd49f7788
Caleb Bassi <calebjbassi@gmail.com>
2017-07-09 23:19:05 -0700

    Update and rename arch_install.sh to installation/arch1.sh

--- installation/arch1.sh
+++ installation/arch1.sh
@@ -47,6 +47,8 @@ pacstrap /mnt \
 # Fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
+curl https://raw.githubusercontent.com/cjbassi/config/master/installation/arch2.sh > /mnt/arch2.sh
+
 # Chroot
 arch-chroot /mnt
 
