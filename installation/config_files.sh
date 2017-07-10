024ad0f0a5ad7916aa83c48b5e55954bd543b59d
Caleb Bassi <calebjbassi@gmail.com>
2017-07-10 00:33:00 -0700

    Update config_files.sh

--- installation/config_files.sh
+++ installation/config_files.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 # Vim
-mkdir ~/.vim/temp ~/.vim/undodir
+mkdir -p ~/.vim/temp ~/.vim/undodir
 
 if ! [[ -e ~/.vim/autoload/plug.vim ]]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 # Ranger
+mkdir -p ~/.config/ranger
 ln -sf ~/config/ranger/rc.conf ~/.config/ranger/rc.conf
 ln -sf ~/config/ranger/commands.py ~/.config/ranger/commands.py
 
 # wm
+mkdir -p ~/.config/i3
 ln -sf ~/config/wm/config ~/.config/i3/
-ln -sf ~/config/wm/opacity.ds ~/.devilspie/
+mkdir -p ~/.
+ln -sf ~/config/wm/opacity.ds ~/.devilspie
 
 # X11 mouse acceleration and speed
 sudo ln -sf ~/config/peripherals/50-mouse.conf /etc/X11/xorg.conf.d/
@@ -22,4 +25,5 @@ sudo ln -sf ~/config/peripherals/50-mouse.conf /etc/X11/xorg.conf.d/
 ln -sf ~/config/dotfiles/.* ~/
 
 # polybar
+mkdir -p ~/.config/polybar
 ln -sf ~/config/polybar/config ~/.config/polybar/
