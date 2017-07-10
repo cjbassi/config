5732ae6f63155f477ae3c6b55de0e2c8cebc1e91
Caleb Bassi <calebjbassi@gmail.com>
2017-07-10 00:05:01 -0700

    Create arch3.sh

---
+++ installation/arch3.sh
@@ -0,0 +1,29 @@
+rm /home/cbassi/arch2.sh
+
+cd ~
+
+mkdir Downloads
+
+sudo pip install glances
+
+git clone https://github.com/cjbassi/config
+bash config/installation/config_files.sh
+bash config/pacaur.sh
+
+pacaur -y\
+discord\
+gitflow-avh\
+gitflow-zshcompletion-avh\
+google-chrome\
+i3lock-color-git\
+neofetch\
+polybar\
+unclutter-xfixes-git\
+universal-ctags-git
+
+# clone urxvt-vim-scrollback
+# install vim mardown preview
+
+#i3-gaps
+#zsh-fast-syntax-highlighting
+#gitflow-avh vs gitflow-avh-git
