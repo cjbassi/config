917b05bf1a84aadcecdd0fabba6cd55a7562b732
Caleb Bassi <calebjbassi@gmail.com>
2017-08-26 11:53:37 -0700

    Update pre_install.sh

--- arch/pre_install.sh
+++ arch/pre_install.sh
@@ -7,6 +7,5 @@ wifi-menu
 mount -L ROOT /mnt
 mount -L BOOT /mnt/boot
 rm -rf /mnt
-curl -L https://goo.gl/fnq2ve > arch1.sh
-https://goo.gl/J38pUW
+curl -L https://goo.gl/J38pUW > arch1.sh
 bash arch1.sh
