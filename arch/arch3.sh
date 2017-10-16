391d27b9ea41704c984334108f9b29a05d4f0e65
Caleb Bassi <calebjbassi@gmail.com>
2017-10-16 14:09:25 -0700

    Update arch3.sh

--- arch/arch3.sh
+++ arch/arch3.sh
@@ -85,13 +85,13 @@ cd ..
 rm -rf vimpager
 cd ~
 
-cd Downloads
-git clone https://github.com/powerline/fonts.git
-cd fonts
-./install.sh
-cd ..
-rm -rf fonts
-cd ~
+#cd Downloads
+#git clone https://github.com/powerline/fonts.git
+#cd fonts
+#./install.sh
+#cd ..
+#rm -rf fonts
+#cd ~
 
 # pip and npm {{{1
 
