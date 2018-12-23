35001f6652cc7aad7f4d9f2ed58839862f30038a
Caleb Bassi <calebjbassi@gmail.com>
2018-12-23 03:09:00 -0800

    Update pre_install.sh

--- installation/pre_install.sh
+++ installation/pre_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 wifi-menu
-curl "https://raw.githubusercontent.com/cjbassi/config/mater/installation/install1.sh" > install1.sh
+curl "https://raw.githubusercontent.com/cjbassi/config/master/installation/install1.sh" > install1.sh
 cat install1.sh
 bash install1.sh
