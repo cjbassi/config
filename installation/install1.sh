c3b254cd997005468145056bd5a26c5049a61611
Caleb Bassi <calebjbassi@gmail.com>
2020-08-26 17:28:42 -0700

    remove partition, boot loader, and encryption from installation

--- installation/install1.sh
+++ installation/install1.sh
@@ -1,26 +1,5 @@
 #!/usr/bin/env bash
 
-# Pre-installation {{{1
-
-# Partition
-# gdisk /dev/sda
-# create new partition table
-# boot: 512 MiB EFI (EF00)
-# root: rest is Linux filesystem (8300)
-
-# Encrypted root partition
-# cryptsetup -y -v luksFormat --type luks2 /dev/sda2
-cryptsetup open /dev/sda2 cryptroot
-# mkfs.ext4 /dev/mapper/cryptroot
-mount /dev/mapper/cryptroot /mnt
-
-# Boot partition
-# mkfs.fat -F32 /dev/sda1
-# mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
-
-rm -rf /mnt
-
 # Packages {{{1
 
 pacstrap /mnt \
--- installation/install2.sh
+++ installation/install2.sh
@@ -1,39 +1,5 @@
 #!/usr/bin/env bash
 
-# Configure the System {{{1
-
-# Change mkinitcpio because of encryption {{{2
-
-SEARCH="^HOOKS=(.*)$"
-REPLACE="HOOKS=(base udev autodetect keyboard keymap consolefont modconf block encrypt filesystems fsck)"
-perl -i -pe "s/$SEARCH/$REPLACE/g" /etc/mkinitcpio.conf
-
-mkinitcpio -p linux
-
-
-# Boot loader {{{2
-
-bootctl --path=/boot install
-
-echo "\
-default arch
-editor  0" \
-> /boot/loader/loader.conf
-
-UUID=$(blkid /dev/sda2 | awk '{print $2}' | sed 's/.*"\(.*\)"/\1/')
-
-echo "\
-title   Arch Linux
-linux   /vmlinuz-linux
-initrd  /intel-ucode.img
-initrd  /initramfs-linux.img
-options cryptdevice=UUID=${UUID}:cryptroot root=/dev/mapper/cryptroot quiet rw" \
-> /boot/loader/entries/arch.conf
-
-# TODO
-# options     root=PARTLABEL=ROOT rw" \
-
-
 # Post-installation {{{1
 
 # Auto login {{{2
