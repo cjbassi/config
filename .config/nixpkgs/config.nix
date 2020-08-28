2a52b5e765af08c6306c6035445ed6187edea141
Caleb Bassi <calebjbassi@gmail.com>
2020-08-27 22:26:34 -0700

    nix-env: add NUR support

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,3 +1,16 @@
+let
+
+  nur-repo = builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz";
+
+in
+
 {
   allowUnfree = true;
+
+  # https://github.com/nix-community/NUR
+  packageOverrides = pkgs: {
+    nur = import nur-repo {
+      inherit pkgs;
+    };
+  };
 }
