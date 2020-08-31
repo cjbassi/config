20748020c022eabd9ef47830d58672fc3f9e8275
Caleb Bassi <calebjbassi@gmail.com>
2020-08-30 17:07:14 -0700

    work on pre-install.sh

--- installation/pre-install.sh
+++ installation/pre-install.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/env bash
 
-wifi-menu
-curl "https://raw.githubusercontent.com/cjbassi/config/master/installation/install1.sh" > install1.sh
-cat install1.sh
-bash install1.sh
+systemctl start display-manager
+
+# Partitioning
+parted /dev/sda -- mklabel gpt
+parted /dev/sda -- mkpart primary 512MiB
+parted /dev/sda -- mkpart ESP fat32 1MiB 512MiB
+parted /dev/sda -- set 2 boot on
+
+# TODO
+# parted /dev/sda -- name 1 nixos
+# parted /dev/sda -- name 2 boot
+
+# Encryption
+cryptsetup luksFormat /dev/disk/by-label/nixos
+cryptsetup luksOpen /dev/disk/by-label/nixos crypted
+
+# Formatting
+mkfs.ext4 /dev/mapper/crypted
+mkfs.fat -F 32 -n boot /dev/sda2
+
+# Installing
+mount /dev/disk/by-label/nixos /mnt
+mkdir -p /mnt/boot
+mount /dev/disk/by-label/boot /mnt/boot
+nixos-generate-config --root /mnt
+nixos-install
+
+reboot
