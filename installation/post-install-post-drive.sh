735bc56a82668da7a3d962e390b2b095f7f35d5c
Caleb Bassi <calebjbassi@gmail.com>
2020-08-31 13:50:56 -0700

    post-install: add cargo credentials

--- installation/post-install-post-drive.sh
+++ installation/post-install-post-drive.sh
@@ -6,6 +6,9 @@ ln -s ~/config ~/Drive/linux/config
 # link .authinfo from Drive
 ln -s ~/Drive/linux/.authinfo ~/.authinfo
 
+# link cargo-credentials from Drive
+ln -s ~/Drive/linux/cargo-credentials ~/.local/share/cargo/credentials
+
 # link .config/hub from Drive
 ln -s ~/Drive/linux/hub ~/.config/hub
 
